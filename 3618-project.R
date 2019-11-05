26eb46bbb6866ea77005680cc32d6d8441d63576
zxjchong <57380739+zxjchong@users.noreply.github.com>
2019-11-04 22:19:02 -0500

    Add files via upload

---
+++ 3618-project.R
@@ -0,0 +1,5 @@
+goblin <- c(1,2,3,4,5,6);goblin 
+werewolf <- c(1,2,3,4,5,6);werewolf 
+mymatrix <- rbind(goblin, werewolf);mymatrix
+colnames(mymatrix) <- c("chocolate","gummies","licorice","lollipops","chewing_gums","cotton_candy")
+rownames(mymatrix)<- c("goblin", "werewolf");mymatrix
